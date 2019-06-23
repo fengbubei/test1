27f9de290cb95c9195a14fab4bac8f8e217fa29b
风 <2523031871@qq.com>
2019-06-23 10:30:18 +0800

    Initial commit

---
+++ src/com/zhognxing/test/HelloAction.java
@@ -0,0 +1,13 @@
+package com.zhognxing.test;
+
+public class HelloAction {
+
+    public String execute(){
+        return "ok";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("xxxxxxxxxxxxxxxxxxxx");
+    }
+
+}
